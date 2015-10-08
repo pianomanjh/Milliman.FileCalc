e2111cd14661a05719b399c752f56824b236303b
Josh Huber <josh.huber@pdp.com>
2015-10-07 19:51:47 -0700

    initial

---
+++ Milliman.FileCalc.Test/CalcTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Milliman.FileCalc.Test
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Milliman.FileCalc/Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milliman.FileCalc
+{
+    public class Calculator
+    {
+    }
+
+    public interface IOperation
+    {
+        decimal Calc(IEnumerable<decimal> values);
+    }
+
+    public class Min : IOperation
+    {
+        public decimal Calc(IEnumerable<decimal> values)
+        {
+            return values.Min();
+        }
+    }
+
+    public class Max : IOperation
+    {
+        public decimal Calc(IEnumerable<decimal> values)
+        {
+            return values.Max();
+        }
+    }
+}
